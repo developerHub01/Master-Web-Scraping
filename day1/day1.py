234f3c45647bac7beac1a5fa8a834a5c80279b04
developerHub01 <shakil102043@gmail.com>
2026-02-21 07:52:00 +0600

    refactor and optimized scrapper

--- day1/day1.py
+++ day1/day1.py
@@ -2,212 +2,240 @@ from random import uniform
 from time import sleep
 from urllib.parse import urljoin
 
-from playwright.sync_api import sync_playwright, Page
 from playwright.sync_api import Error
 import sqlite3
 
-WEBSITE_URL = "https://books.toscrape.com"
 
-rating_map = {
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-}
+class Scrapper:
+    WEBSITE_URL = "https://books.toscrape.com"
 
-product_info_map = {
-    "UPC": "upc",
-    "Product Type": "product_type",
-    "Price (excl. tax)": "price_without_tax",
-    "Price (incl. tax)": "price_with_tax",
-    "Tax": "tax",
-    "Availability": "availability",
-    "Number of reviews": "number_of_reviews",
-}
+    RATING_MAP = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+    }
 
+    PRODUCT_INFO_MAP = {
+        "UPC": "upc",
+        "Product Type": "product_type",
+        "Price (excl. tax)": "price_without_tax",
+        "Price (incl. tax)": "price_with_tax",
+        "Tax": "tax",
+        "Availability": "availability",
+        "Number of reviews": "number_of_reviews",
+    }
 
-def search_books_page(db_cursor: sqlite3.Cursor, db_conn: sqlite3.Connection, page: Page, index: int = 0):
-    print(f"page number === {index + 1}")
-    print(page.url)
-    books = []
+    def __init__(self):
+        self.db_conn = sqlite3.connect("books.db")
+        self.db_cursor = self.db_conn.cursor()
+        self.browser = None
+        self.page = None
+        self.setup_db()
 
-    page.wait_for_load_state("domcontentloaded")
-    page.wait_for_selector(".product_pod")
-    books_locator = page.locator(".product_pod").all()
+    def setup_db(self):
+        self.db_cursor.execute("""
+            CREATE TABLE IF NOT EXISTS books(
+                link TEXT PRIMARY KEY,
+                name TEXT,
+                price_with_tax REAL,
+                price_without_tax REAL,
+                tax REAL,
+                rating INTEGER,
+                thumbnail TEXT,
+                image TEXT,
+                category TEXT,
+                description TEXT,
+                upc TEXT,
+                product_type TEXT,
+                availability INTEGER,
+                number_of_reviews INTEGER
+        )""")
+        self.db_conn.commit()
 
-    for book in books_locator:
-        sub_url = book.locator("h3 a").get_attribute("href")
-        if not sub_url:
-            print(f"""
-            no url found 
-            ===>>  {sub_url}
-            """)
-            continue
-        sub_url = sub_url.strip()
+    def run(self):
+        from playwright.sync_api import sync_playwright
 
-        sub_url = urljoin(page.url, sub_url)
-        print(sub_url)
-        thumbnail = book.locator(".image_container img").first.get_attribute("src")
-        thumbnail = urljoin(WEBSITE_URL, thumbnail) if thumbnail else None
+        with sync_playwright() as p:
+            self.browser = p.chromium.launch(headless=True)
 
-        books.append((sub_url, thumbnail))
+            self.page = self.browser.new_page()
 
-    db_cursor.executemany("""
-        insert or ignore into books (link, thumbnail) values (?, ?)
-    """, books)
-    db_conn.commit()
+            # self.page.route("**/*.{png,jpg,jpeg,webp,gif,svg}", lambda route: route.abort())
+            # self.page.route("**/*.{woff,woff2,ttf}", lambda route: route.abort())
 
-    try:
-        next_locator = page.locator(".pager .next a")
-        if not next_locator.count():
-            raise Error("")
+            self.page.goto(self.WEBSITE_URL, wait_until="domcontentloaded")
 
-        page.evaluate("window.scrollBy(0, window.innerHeight)")
-        sleep(uniform(1, 3.5))
+            self.search_books_page()
+            print("books link listing completed...........")
+            self.get_books_details()
+            print("books details listing completed...........")
+            print("""===========================================
+            ================ MISSION ACCOMPLISED =================
+            ===========================================""")
 
-        next_locator.click()
-        search_books_page(db_cursor=db_cursor, db_conn=db_conn, page=page, index=index + 1)
-    except Error:
-        print("no next page")
+            self.preview_data()
 
+            self.browser.close()
+            self.db_conn.close()
 
-def get_books_details(db_cursor: sqlite3.Cursor, db_conn: sqlite3.Connection, page: Page):
-    rows = list(map(lambda x: x[0], db_cursor.execute("""
-        select link from books
-    """).fetchall()))
-    for index, url in enumerate(rows):
-        search_by_url(db_cursor=db_cursor, db_conn=db_conn, page=page, url=url)
-        print(f"{index + 1} / {len(rows)}")
+    def search_books_page(self, index: int = 0):
+        print(f"page number === {index + 1}")
+        print(self.page.url)
+        books = []
 
+        self.page.wait_for_load_state("domcontentloaded")
+        self.page.wait_for_selector(".product_pod")
+        books_locator = self.page.locator(".product_pod").all()
 
-def search_by_url(db_cursor: sqlite3.Cursor, db_conn: sqlite3.Connection, page: Page, url: str):
-    print(url)
-    try:
-        book_details = {}
-        page.goto(url, wait_until="domcontentloaded")
-        page.wait_for_selector(".page_inner")
+        for book in books_locator:
+            sub_url = book.locator("h3 a").get_attribute("href")
+            if not sub_url:
+                print(f"""
+                no url found 
+                ===>>  {sub_url}
+                """)
+                continue
+            sub_url = sub_url.strip()
 
-        if not page.locator(".page_inner").count():
+            sub_url = urljoin(self.page.url, sub_url)
+            print(sub_url)
+            thumbnail = book.locator(".image_container img").first.get_attribute("src")
+            thumbnail = urljoin(self.WEBSITE_URL, thumbnail) if thumbnail else None
+
+            books.append((sub_url, thumbnail))
+
+        self.db_cursor.executemany("""
+            INSERT OR IGNORE INTO books (link, thumbnail) VALUES (?, ?)
+        """, books)
+        self.db_conn.commit()
+
+        try:
+            next_locator = self.page.locator(".pager .next a")
+            if not next_locator.count():
+                raise Error("")
+
+            self.page.evaluate("window.scrollBy(0, window.innerHeight)")
+            sleep(uniform(1, 3.5))
+
+            next_locator.click()
+            self.search_books_page(index=index + 1)
+        except Error:
+            print("no next page")
+
+    def get_books_details(self):
+        rows = list(map(lambda x: x[0], self.db_cursor.execute("""
+            SELECT link FROM books
+        """).fetchall()))
+        for index, url in enumerate(rows):
+            self.search_by_url(url=url)
+            print(f"{index + 1} / {len(rows)}")
+
+    def search_by_url(self, url: str):
+        print(url)
+        try:
+            book_details = {}
+            self.page.goto(url, wait_until="domcontentloaded")
+            self.page.wait_for_selector(".page_inner")
+
+            if not self.page.locator(".page_inner").count():
+                try:
+                    self.db_cursor.execute(f"DELETE FROM books WHERE link = '{url}'")
+                    self.db_conn.commit()
+                except sqlite3.Error:
+                    pass
+                return
+
             try:
-                db_cursor.execute(f"delete from books where link = '{url}'")
-                db_conn.commit()
-            except sqlite3.Error:
-                pass
-            return
+                self.page.wait_for_selector(".breadcrumb li a")
+                book_details["category"] = self.page.locator(".breadcrumb li a").nth(2).inner_text().strip()
+            except Error:
+                print("category not found")
 
-        try:
-            page.wait_for_selector(".breadcrumb li a")
-            book_details["category"] = page.locator(".breadcrumb li a").nth(2).inner_text().strip()
+            try:
+                self.page.wait_for_selector(".product_page .product_main h1")
+                book_details["name"] = self.page.locator(".product_page .product_main h1").inner_text()
+            except Error:
+                print("name not found")
+
+            try:
+                self.page.wait_for_selector("#product_gallery .thumbnail .active img")
+                src = self.page.locator("#product_gallery .thumbnail .active img").get_attribute(
+                    "src").strip()
+                if not src: raise Error("")
+                book_details["image"] = urljoin(self.WEBSITE_URL, src)
+            except Error:
+                print("image not found")
+
+            try:
+                self.page.wait_for_selector("#product_description+p")
+                book_details["description"] = self.page.locator("#product_description+p").inner_text().strip()
+            except Error:
+                print("description not found")
+
+            try:
+                self.page.wait_for_selector(".star-rating")
+                rating = self.page.locator(".star-rating").first.get_attribute("class").split(" ")[-1].lower().strip()
+                if rating not in self.RATING_MAP: raise Error("")
+                book_details["rating"] = self.RATING_MAP[rating]
+            except Error:
+                print("rating not found")
+
+            try:
+                self.page.wait_for_selector("table.table.table-striped tr")
+                book_info_rows = self.page.locator("table.table.table-striped tr").all()
+                for row in book_info_rows:
+                    key = row.locator("th").inner_text()
+                    if not key: continue
+                    key = self.PRODUCT_INFO_MAP[key.strip()]
+
+                    value = row.locator("td").inner_text()
+                    value = value.strip() if value else None
+
+                    if not key:
+                        raise Error("")
+
+                    if key == "number_of_reviews":
+                        book_details[key] = int(value) if value else 0
+                    elif key in ["price_without_tax", "price_with_tax", "tax"]:
+                        value = value.removeprefix("£")
+                        book_details[key] = float(value) if value else None
+                    elif key == "availability":
+                        value = value.removeprefix("In stock (").removesuffix(" available)")
+                        book_details[key] = float(value) if value else 0
+                    else:
+                        book_details[key] = value
+
+            except Error:
+                print("UPC not found")
+
+            updates = []
+            values = []
+            for key, value in book_details.items():
+                updates.append(f"{key} = ?")
+                values.append(value)
+
+            values.append(url)
+            self.db_cursor.execute(f"""
+                UPDATE books SET {", ".join(updates)} WHERE link = ?
+            """, values)
+            self.db_conn.commit()
         except Error:
-            print("category not found")
+            print(Error)
 
-        try:
-            page.wait_for_selector(".product_page .product_main h1")
-            book_details["name"] = page.locator(".product_page .product_main h1").inner_text()
-        except Error:
-            print("name not found")
+    def preview_data(self, limit: int = 5):
+        rows = self.db_cursor.execute(f"SELECT * FROM books LIMIT ?", (limit,)).fetchall()
+        print(self.db_cursor.description)
+        columns = [desc[0] for desc in self.db_cursor.description]
+        data_list = [dict(zip(columns, row)) for row in rows]
 
-        try:
-            page.wait_for_selector("#product_gallery .thumbnail .active img")
-            src = page.locator("#product_gallery .thumbnail .active img").get_attribute(
-                "src").strip()
-            if not src: raise Error("")
-            book_details["image"] = urljoin(WEBSITE_URL, src)
-        except Error:
-            print("image not found")
+        import json
+        print(json.dumps(data_list, indent=4))
 
-        try:
-            page.wait_for_selector("#product_description+p")
-            book_details["description"] = page.locator("#product_description+p").inner_text().strip()
-        except Error:
-            print("description not found")
-
-        try:
-            page.wait_for_selector(".star-rating")
-            rating = page.locator(".star-rating").first.get_attribute("class").split(" ")[-1].lower().strip()
-            if rating not in rating_map: raise Error("")
-            book_details["rating"] = rating_map[rating]
-        except Error:
-            print("rating not found")
-
-        try:
-            page.wait_for_selector("table.table.table-striped tr")
-            book_info_rows = page.locator("table.table.table-striped tr").all()
-            for row in book_info_rows:
-                key = product_info_map[row.locator("th").inner_text().strip()]
-                value = row.locator("td").inner_text().strip()
-                if not key: raise Error("")
-
-                if key == "number_of_reviews":
-                    book_details[key] = int(value) if value else 0
-                elif key in ["price_without_tax", "price_with_tax", "tax"]:
-                    value = value.removeprefix("£")
-                    book_details[key] = float(value) if value else None
-                elif key == "availability":
-                    value = value.removeprefix("In stock (").removesuffix(" available)")
-                    book_details[key] = float(value) if value else 0
-                else:
-                    book_details[key] = value
-
-        except Error:
-            print("UPC not found")
-
-        updates = []
-        values = []
-        for key, value in book_details.items():
-            updates.append(f"{key} = ?")
-            values.append(value)
-
-        values.append(url)
-        db_cursor.execute(f"""
-            update books set {", ".join(updates)} where link = ?
-        """, values)
-        db_conn.commit()
-    except Error:
-        print(Error)
-
-
-def main():
-    with sync_playwright() as p:
-        db_conn = sqlite3.connect("books.db")
-        cursor = db_conn.cursor()
-
-        cursor.execute("""
-            create table if not exists books(
-                link text primary key,
-                name text,
-                price_with_tax real,
-                price_without_tax real,
-                tax real,
-                rating integer,
-                thumbnail text,
-                image text,
-                category text,
-                description text,
-                upc text,
-                product_type text,
-                availability integer,
-                number_of_reviews integer
-            )""")
-        db_conn.commit()
-
-        browser = p.chromium.launch(headless=True)
-
-        page = browser.new_page()
-        page.goto(WEBSITE_URL, wait_until="domcontentloaded")
-
-        # search_books_page(db_cursor=cursor, db_conn=db_conn, page=page)
-        # print("books link listing completed...........")
-        # get_books_details(db_cursor=cursor, db_conn=db_conn, page=page)
-        # print("books details listing completed...........")
-        # print("""===========================================
-        # ================ MISSION ACCOMPLISED =================
-        # ===========================================""")
-
-        browser.close()
-        db_conn.close()
+        return data_list
 
 
 if __name__ == "__main__":
-    main()
+    scrapper = Scrapper()
+    scrapper.run()
---
+++ day1/readme.md
@@ -0,0 +1,90 @@
+## 1000 data from toscrape
+
+https://books.toscrape.com/
+
+## Example output
+
+```json
+[
+  {
+    "link": "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html",
+    "name": "A Light in the Attic",
+    "price_with_tax": 51.77,
+    "price_without_tax": 51.77,
+    "tax": 0.0,
+    "rating": 3,
+    "thumbnail": "https://books.toscrape.com/media/cache/2c/da/2cdad67c44b002e7ead0cc35693c0e8b.jpg",
+    "image": "https://books.toscrape.com/media/cache/fe/72/fe72f0532301ec28892ae79a629a293c.jpg",
+    "category": "Poetry",
+    "description": "It's hard to imagine a world without A Light in the Attic. This now-classic collection of poetry and drawings from Shel Silverstein celebrates its 20th anniversary with this special edition. Silverstein's humorous and creative verse can amuse the dowdiest of readers. Lemon-faced adults and fidgety kids sit still and read these rhythmic words and laugh and smile and love th It's hard to imagine a world without A Light in the Attic. This now-classic collection of poetry and drawings from Shel Silverstein celebrates its 20th anniversary with this special edition. Silverstein's humorous and creative verse can amuse the dowdiest of readers. Lemon-faced adults and fidgety kids sit still and read these rhythmic words and laugh and smile and love that Silverstein. Need proof of his genius? RockabyeRockabye baby, in the treetopDon't you know a treetopIs no safe place to rock?And who put you up there,And your cradle, too?Baby, I think someone down here'sGot it in for you. Shel, you never sounded so good. ...more",
+    "upc": "a897fe39b1053632",
+    "product_type": "Books",
+    "availability": 22,
+    "number_of_reviews": 0
+  },
+  {
+    "link": "https://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html",
+    "name": "Tipping the Velvet",
+    "price_with_tax": 53.74,
+    "price_without_tax": 53.74,
+    "tax": 0.0,
+    "rating": 1,
+    "thumbnail": "https://books.toscrape.com/media/cache/26/0c/260c6ae16bce31c8f8c95daddd9f4a1c.jpg",
+    "image": "https://books.toscrape.com/media/cache/08/e9/08e94f3731d7d6b760dfbfbc02ca5c62.jpg",
+    "category": "Historical Fiction",
+    "description": "\"Erotic and absorbing...Written with starling power.\"--\"The New York Times Book Review \" Nan King, an oyster girl, is captivated by the music hall phenomenon Kitty Butler, a male impersonator extraordinaire treading the boards in Canterbury. Through a friend at the box office, Nan manages to visit all her shows and finally meet her heroine. Soon after, she becomes Kitty's \"Erotic and absorbing...Written with starling power.\"--\"The New York Times Book Review \" Nan King, an oyster girl, is captivated by the music hall phenomenon Kitty Butler, a male impersonator extraordinaire treading the boards in Canterbury. Through a friend at the box office, Nan manages to visit all her shows and finally meet her heroine. Soon after, she becomes Kitty's dresser and the two head for the bright lights of Leicester Square where they begin a glittering career as music-hall stars in an all-singing and dancing double act. At the same time, behind closed doors, they admit their attraction to each other and their affair begins. ...more",
+    "upc": "90fa61229261140a",
+    "product_type": "Books",
+    "availability": 20,
+    "number_of_reviews": 0
+  },
+  {
+    "link": "https://books.toscrape.com/catalogue/soumission_998/index.html",
+    "name": "Soumission",
+    "price_with_tax": 50.1,
+    "price_without_tax": 50.1,
+    "tax": 0.0,
+    "rating": 1,
+    "thumbnail": "https://books.toscrape.com/media/cache/3e/ef/3eef99c9d9adef34639f510662022830.jpg",
+    "image": "https://books.toscrape.com/media/cache/ee/cf/eecfe998905e455df12064dba399c075.jpg",
+    "category": "Fiction",
+    "description": "Dans une France assez proche de la n\u00f4tre, un homme s\u2019engage dans la carri\u00e8re universitaire. Peu motiv\u00e9 par l\u2019enseignement, il s\u2019attend \u00e0 une vie ennuyeuse mais calme, prot\u00e9g\u00e9e des grands drames historiques. Cependant les forces en jeu dans le pays ont fissur\u00e9 le syst\u00e8me politique jusqu\u2019\u00e0 provoquer son effondrement. Cette implosion sans soubresauts, sans vraie r\u00e9volution, s Dans une France assez proche de la n\u00f4tre, un homme s\u2019engage dans la carri\u00e8re universitaire. Peu motiv\u00e9 par l\u2019enseignement, il s\u2019attend \u00e0 une vie ennuyeuse mais calme, prot\u00e9g\u00e9e des grands drames historiques. Cependant les forces en jeu dans le pays ont fissur\u00e9 le syst\u00e8me politique jusqu\u2019\u00e0 provoquer son effondrement. Cette implosion sans soubresauts, sans vraie r\u00e9volution, se d\u00e9veloppe comme un mauvais r\u00eave.Le talent de l\u2019auteur, sa force visionnaire nous entra\u00eenent sur un terrain ambigu et glissant ; son regard sur notre civilisation vieillissante fait coexister dans ce roman les intuitions po\u00e9tiques, les effets comiques, une m\u00e9lancolie fataliste.Ce livre est une saisissante fable politique et morale. ...more",
+    "upc": "6957f44c3847a760",
+    "product_type": "Books",
+    "availability": 20,
+    "number_of_reviews": 0
+  },
+  {
+    "link": "https://books.toscrape.com/catalogue/sharp-objects_997/index.html",
+    "name": "Sharp Objects",
+    "price_with_tax": 47.82,
+    "price_without_tax": 47.82,
+    "tax": 0.0,
+    "rating": 4,
+    "thumbnail": "https://books.toscrape.com/media/cache/32/51/3251cf3a3412f53f339e42cac2134093.jpg",
+    "image": "https://books.toscrape.com/media/cache/c0/59/c05972805aa7201171b8fc71a5b00292.jpg",
+    "category": "Mystery",
+    "description": "WICKED above her hipbone, GIRL across her heart Words are like a road map to reporter Camille Preaker\u2019s troubled past. Fresh from a brief stay at a psych hospital, Camille\u2019s first assignment from the second-rate daily paper where she works brings her reluctantly back to her hometown to cover the murders of two preteen girls. NASTY on her kneecap, BABYDOLL on her leg Since WICKED above her hipbone, GIRL across her heart Words are like a road map to reporter Camille Preaker\u2019s troubled past. Fresh from a brief stay at a psych hospital, Camille\u2019s first assignment from the second-rate daily paper where she works brings her reluctantly back to her hometown to cover the murders of two preteen girls. NASTY on her kneecap, BABYDOLL on her leg Since she left town eight years ago, Camille has hardly spoken to her neurotic, hypochondriac mother or to the half-sister she barely knows: a beautiful thirteen-year-old with an eerie grip on the town. Now, installed again in her family\u2019s Victorian mansion, Camille is haunted by the childhood tragedy she has spent her whole life trying to cut from her memory. HARMFUL on her wrist, WHORE on her ankle As Camille works to uncover the truth about these violent crimes, she finds herself identifying with the young victims\u2014a bit too strongly. Clues keep leading to dead ends, forcing Camille to unravel the psychological puzzle of her own past to get at the story. Dogged by her own demons, Camille will have to confront what happened to her years before if she wants to survive this homecoming.With its taut, crafted writing, Sharp Objects is addictive, haunting, and unforgettable. ...more",
+    "upc": "e00eb4fd7b871a48",
+    "product_type": "Books",
+    "availability": 20,
+    "number_of_reviews": 0
+  },
+  {
+    "link": "https://books.toscrape.com/catalogue/sapiens-a-brief-history-of-humankind_996/index.html",
+    "name": "Sapiens: A Brief History of Humankind",
+    "price_with_tax": 54.23,
+    "price_without_tax": 54.23,
+    "tax": 0.0,
+    "rating": 5,
+    "thumbnail": "https://books.toscrape.com/media/cache/be/a5/bea5697f2534a2f86a3ef27b5a8c12a6.jpg",
+    "image": "https://books.toscrape.com/media/cache/ce/5f/ce5f052c65cc963cf4422be096e915c9.jpg",
+    "category": "History",
+    "description": "From a renowned historian comes a groundbreaking narrative of humanity\u2019s creation and evolution\u2014a #1 international bestseller\u2014that explores the ways in which biology and history have defined us and enhanced our understanding of what it means to be \u201chuman.\u201dOne hundred thousand years ago, at least six different species of humans inhabited Earth. Yet today there is only one\u2014h From a renowned historian comes a groundbreaking narrative of humanity\u2019s creation and evolution\u2014a #1 international bestseller\u2014that explores the ways in which biology and history have defined us and enhanced our understanding of what it means to be \u201chuman.\u201dOne hundred thousand years ago, at least six different species of humans inhabited Earth. Yet today there is only one\u2014homo sapiens. What happened to the others? And what may happen to us?Most books about the history of humanity pursue either a historical or a biological approach, but Dr. Yuval Noah Harari breaks the mold with this highly original book that begins about 70,000 years ago with the appearance of modern cognition. From examining the role evolving humans have played in the global ecosystem to charting the rise of empires, Sapiens integrates history and science to reconsider accepted narratives, connect past developments with contemporary concerns, and examine specific events within the context of larger ideas.Dr. Harari also compels us to look ahead, because over the last few decades humans have begun to bend laws of natural selection that have governed life for the past four billion years. We are acquiring the ability to design not only the world around us, but also ourselves. Where is this leading us, and what do we want to become?Featuring 27 photographs, 6 maps, and 25 illustrations/diagrams, this provocative and insightful work is sure to spark debate and is essential reading for aficionados of Jared Diamond, James Gleick, Matt Ridley, Robert Wright, and Sharon Moalem. ...more",
+    "upc": "4165285e1663650f",
+    "product_type": "Books",
+    "availability": 20,
+    "number_of_reviews": 0
+  }
+]
+```
